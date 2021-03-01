8c9a34d71c307e42c623c3e1933cfe91c40a0844
richardsimbana <>
2021-02-28 19:08:01 -0500

    adding health endpoint

--- src/api/routes/ProfileRouter.js
+++ src/api/routes/ProfileRouter.js
@@ -5,26 +5,32 @@ const router = express.Router();
 router.get('/api/profiles', async (req, res) => {
     try {
         const profiles = await profileController.getAllProfiles();
-        res.status(201).json({
+        return res.status(201).json({
             message: 'Get all success',
             profiles
         })
     } catch (error) {
-        res.status(400).send();
+        return res.status(400).send();
     }
 });
 
 router.get('/api/profiles/:name', async (req, res) => {
     try {
         const profile = await profileController.getProfileByName(req.params.name);
-        res.status(200).json({
+        return res.status(200).json({
             message: 'Profile by name was fetched',
             profile
         });
     } catch (err) {
-        res.status(400).send();
+        return res.status(400).send();
     }
     
+});
+
+router.get('/health', async (req, res) => {
+    return res.status(200).json({
+        message: 'healthy'
+    })
 });
 
 module.exports = router;
